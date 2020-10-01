ab7292207674fa45702b7d23fd5a15803218c844
Vaeru-s <drakocha-1-2@mail.ru>
2020-10-01 11:55:26 +0300

    AddFirstController

---
+++ src/Controller/User/HomeController.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace App\Controller\User;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController{
+
+     /**
+         * @Route("/", name="home")
+        */
+
+    public function index()
+    {
+        return $this->render('user/index.html.twig', [
+            'title' => 'Главная страница',
+        ]);
+    }
+
+}
